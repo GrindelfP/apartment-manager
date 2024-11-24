d21ae178531f2e97557537d4faba26158adf5b75
GrindelfP <grindelf.perlomutrovij@gmail.com>
2024-11-24 23:18:17 +0300

    test: getByKey, readAll, delete in SQLOperatorTest, bug: update still does not work

--- src/test/java/to/grindelf/apartmentmanager/utils/database/SQLOperatorTest.java
+++ src/test/java/to/grindelf/apartmentmanager/utils/database/SQLOperatorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import to.grindelf.apartmentmanager.domain.User;
 import to.grindelf.apartmentmanager.domain.UserStatus;
 
-import java.io.Console;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.sql.Connection;
@@ -14,7 +13,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SQLOperatorTest {
 
@@ -54,7 +53,7 @@ class SQLOperatorTest {
     void insert() throws SQLException {
         SQLOperator<User, String> operator = new SQLOperator<>();
 
-        User user = new User("Ira", "123", UserStatus.ADMIN);
+        User user = new User("Shisha", "456", UserStatus.JUST_USER);
 
         operator.post(
                 user,
@@ -65,10 +64,10 @@ class SQLOperatorTest {
 
     @Test
     @Order(2)
-    void update() throws SQLException {
+    void update() throws SQLException { // not working
         SQLOperator<User, String> operator = new SQLOperator<>();
 
-        User userUpdate = new User("Ira My Ira", "123", UserStatus.JUST_USER);
+        User userUpdate = new User("Ira_My_Ira", "789", UserStatus.JUST_USER);
         Class<?> clazz = userUpdate.getClass();
         Field[] fields = clazz.getFields();
 
@@ -83,11 +82,11 @@ class SQLOperatorTest {
 
     @Test
     @Order(3)
-    void delete() throws SQLException {
+    void delete() throws SQLException { // works but not tells if no such user
         SQLOperator<User, String> operator = new SQLOperator<>();
 
         operator.delete(
-                "Ira",
+                "asdasdasdasd",
                 "name",
                 PATH,
                 DatabaseTableNames.USERS_TABLE
@@ -96,7 +95,7 @@ class SQLOperatorTest {
 
     @Test
     @Order(4)
-    void readAll() throws SQLException {
+    void readAll() throws SQLException { // works
         SQLOperator<User, String> operator = new SQLOperator<>();
 
         List<User> users = operator.getAll(
@@ -105,8 +104,42 @@ class SQLOperatorTest {
                 "users"
         );
 
+        System.out.println(users.size());
+
         for (User user : users) {
             System.out.println(user);
         }
+    }
+
+    @Test
+    @Order(5)
+    void getByKey() throws SQLException {
+        SQLOperator<User, String> operator = new SQLOperator<>();
+
+        User user = operator.getByKey(
+                "Shisha",
+                "name",
+                PATH,
+                DatabaseTableNames.USERS_TABLE,
+                new DatabaseDaoUtils().userMapper
+        );
+
+        System.out.println(user);
+    }
+
+    @Test
+    @Order(5)
+    void getByKeyWithNoSuchRecord() throws SQLException {
+        SQLOperator<User, String> operator = new SQLOperator<>();
+
+        assertThrows(SQLException.class, () -> {
+            operator.getByKey(
+                    "no-such-user",
+                    "name",
+                    PATH,
+                    DatabaseTableNames.USERS_TABLE,
+                    new DatabaseDaoUtils().userMapper
+            );
+        });
     }
 }
